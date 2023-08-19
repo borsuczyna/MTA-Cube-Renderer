2f177b6800482a79169715628875a36c3cc21b87
borsuczyna <libxxerty@gmail.com>
2023-08-19 17:33:26 +0200

    dont reload same texture

--- scripts/skybox.lua
+++ scripts/skybox.lua
@@ -12,16 +12,25 @@ function createSkybox()
 end
 
 function setSkyTexture(index, texture)
-    if skyTextures[index] and isElement(skyTextures[index]) then
-        destroyElement(skyTextures[index])
+    if getSkyTexture(index) == texture then return end
+
+    if skyTextures[index] and isElement(skyTextures[index].texture) then
+        destroyElement(skyTextures[index].texture)
     end
 
-    skyTextures[index] = dxCreateTexture(texture)
-    dxSetShaderValue(shader, 'sSkyTexture' .. (index == 1 and 'A' or 'B'), skyTextures[index])
+    skyTextures[index] = {
+        texture = dxCreateTexture(texture),
+        name = texture
+    }
+    dxSetShaderValue(shader, 'sSkyTexture' .. (index == 1 and 'A' or 'B'), skyTextures[index].texture)
 end
 
 function setSkyInterpolation(value)
     dxSetShaderValue(shader, 'sSkyInterpolation', value)
+end
+
+function getSkyTexture(index)
+    return skyTextures[index] and skyTextures[index].name or false
 end
 
 function updateSkybox()
